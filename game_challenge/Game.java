a11f5f696f8e96b6e75693eb42891a8b574c7835
zedbeit <devbeita@gmail.com>
2022-07-28 15:22:13 +0100

    game challenge start

---
+++ game_challenge/Game.java
@@ -0,0 +1,70 @@
+package game_challenge;
+import java.util.*;
+
+public class Game {
+    static final Scanner sc = new Scanner(System.in);
+    static final int row = 6;
+    static final int col = 7;
+
+    public static char[][] createBoard() {
+        char[][] board = new char[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        return board;
+    }
+
+    // 1.1
+    public static boolean validBoardSqaure(char input) {
+        return input == 'Y' || input == 'R';
+    }
+
+    // 1.2
+    public static boolean validBoard(char[][] board) {
+        int redCounter = 0;
+        int yellowCounter = 0;
+        boolean emptySquareBelow = false;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (board[i][j] == 'R') {
+                    redCounter++;
+                }
+                if (board[i][j] == 'Y') {
+                    yellowCounter++;
+                }
+
+                if (i + 1 < 6) {
+                    if (board[i][j] != '.' && board[i + 1][j] == '.') {
+                        emptySquareBelow = true;
+                    }
+                }
+            }
+        }
+
+        if (redCounter == 0 || yellowCounter == 0 || emptySquareBelow) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // 1.3
+    public static boolean validMove(int column, char[][] board) {
+
+        if (!(column >= 0 && column < 7)) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            if (board[i][column] == '.') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ game_challenge/Main.java
@@ -0,0 +1,11 @@
+package game_challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // char[][] board = Game.createBoard();
+
+        // System.out.print(Game.validBoard(board));
+        // System.out.print(Game.validMove(6, board));
+    }
+}
